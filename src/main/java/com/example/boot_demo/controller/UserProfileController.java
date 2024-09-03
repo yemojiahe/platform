be41b716813e088ad6662f0e4982cc0df7078224
yemojiahe <3330407028@qq.com>
2024-09-03 22:29:59 +0800

    9.3

--- src/main/java/com/example/boot_demo/controller/UserProfileController.java
+++ src/main/java/com/example/boot_demo/controller/UserProfileController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/text")
 public class UserProfileController {
 
     @Autowired
@@ -31,25 +31,28 @@ public class UserProfileController {
     private DictionaryUpdateService dictionaryUpdateService;
 
 
-    //通知客户端开始数据收集
+    //收集数据转发给特征统计模块
     @PostMapping("/notify-data-collection")
     public ResponseEntity<String> notifyDataCollection(@RequestBody DataCollectionRequest request) {
         //DataCollectionRequest 对象的内容来自于每个请求的请求体。不同 IP 发出的请求会分别创建各自的 DataCollectionRequest
         //实例，因此请求之间的数据不会相互覆盖。每次请求都是独立处理的，系统会处理请求体中的数据，而不会将来自不同 IP 的数据混合或覆盖
+
+        String encryptedVector = request.getEncryptedVector();
+        attributeUpdateService.forwardStatistics(encryptedVector);
         return ResponseEntity.ok("Data collection request has been sent.");
     }
 
-    // 数据收集后转发
-    @PostMapping("/upload-data")
-    public ResponseEntity<String> uploadData(@RequestBody CollectedData collectedData) {
-        // 处理上传的数据（数据先接收，然后要转发）
-        attributeUpdateService.processCollectedData();
-        // 从 collectedData 中提取加密向量并转发
-        String encryptedVector = collectedData.getEncryptedVector();
-        attributeUpdateService.forwardStatistics(encryptedVector);
-
-        return ResponseEntity.ok("Data received and processed.");
-    }
+//    // 数据收集后转发
+//    @PostMapping("/upload-data")
+//    public ResponseEntity<String> uploadData(@RequestBody CollectedData collectedData) {
+//        // 处理上传的数据（数据先接收，然后要转发）
+//        attributeUpdateService.processCollectedData();
+//        // 从 collectedData 中提取加密向量并转发
+//        String encryptedVector = collectedData.getEncryptedVector();
+//        attributeUpdateService.forwardStatistics(encryptedVector);
+//
+//        return ResponseEntity.ok("Data received and processed.");
+//    }
 
 
     //向前端更新属性字典
@@ -62,7 +65,6 @@ public class UserProfileController {
         System.out.println(attributes);
         return ResponseEntity.ok(attributes);
     }
-
 
 
 
